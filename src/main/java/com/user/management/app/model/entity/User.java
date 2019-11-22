170e0975cdd2cc6fc7a0e3af9dac11d69587c94c
William Leon <weleonm@gmail.com>
2019-11-21 22:01:12 -0500

    userManagementModule
    Create Controller, Service and repository layers for CRUD over user
    Create Model for User and Rol DB entities
    Implement Liquibase framework for CI tool for DB,
    Revert "userManagementModule"

---
+++ src/main/java/com/user/management/app/model/entity/User.java
@@ -0,0 +1,60 @@
+package com.user.management.app.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * Entity class to retrieve User information from database
+ *
+ * @author <a href="weleonm@gmail.com">William Leon</a>
+ * @version 1.0
+ * @since 1.0
+ */
+@Entity
+@Table(name = "user")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
+
+    @JsonIgnore
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+
+    @JsonIgnore
+    @Column(name="password")
+    private String password;
+
+    @Column(name="user_name")
+    private String userName;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="phone_number")
+    private String phoneNumber;
+
+    @JsonIgnore
+    @Column(name="activated")
+    private boolean activated;
+
+    @JsonIgnore
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(foreignKey = @ForeignKey(name = "fk_user_rol"), name = "rol_id")
+    private Rol rol;
+
+}
